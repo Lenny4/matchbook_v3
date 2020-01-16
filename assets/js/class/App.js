a376dc61f0533830e3177230fe19004e06fb9a27
Lenny4 <alexandre.beaujour@hotmail.fr>
2020-01-16 22:48:36 +0100

    add 8 matchs

--- assets/js/class/App.js
+++ assets/js/class/App.js
@@ -1,11 +1,29 @@
 "use strict";
 
+const match_1 = require('../../../matchs/1');
+// const match_2 = require('../../../matchs/2');
+// const match_3 = require('../../../matchs/3');
+// const match_4 = require('../../../matchs/4');
+// const match_5 = require('../../../matchs/5');
+// const match_6 = require('../../../matchs/6');
+// const match_7 = require('../../../matchs/7');
+// const match_8 = require('../../../matchs/8');
+
 class App {
     constructor() {
+        this.matchs = [];
     }
 
     init() {
-        console.log(1);
+        this.matchs.push(match_1);
+        // this.matchs.push(match_2);
+        // this.matchs.push(match_3);
+        // this.matchs.push(match_4);
+        // this.matchs.push(match_5);
+        // this.matchs.push(match_6);
+        // this.matchs.push(match_7);
+        // this.matchs.push(match_8);
+        console.log(this.matchs);
     }
 }
 
