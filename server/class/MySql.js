abb8665b56d8f5f5f93f33cc4688b319ce3bef33
Lenny4 <alexandre.beaujour@hotmail.fr>
2020-01-22 23:40:58 +0100

    can get match from bdd

---
+++ server/class/MySql.js
@@ -0,0 +1,42 @@
+"use strict";
+const mysql2 = require('mysql2');
+const Const = require('../../Const');
+
+class MySql {
+    constructor() {
+        this.host = Const.mysql.host;
+        this.user = Const.mysql.user;
+        this.password = Const.mysql.password;
+        this.database = Const.mysql.database;
+        this.pool = mysql2.createPool({
+            host: this.host,
+            user: this.user,
+            password: this.password,
+            database: this.database,
+        });
+    }
+
+    getAllEvents(callback) {
+        const sql = `SELECT id, name, eventId, sportId, start FROM event;`;
+        this.pool.execute(
+            sql,
+            [],
+            (err, events, fields) => {
+                if (err) console.log(err);
+                callback(events);
+            }
+        );
+    }
+
+    // insertNotificationNewMessage(notificationId, data) {
+    //     this.pool.execute(
+    //         'INSERT INTO `notification_new_message`(`id`, `sender_id`, `chat_room_id`, `chat_type`, `nb_message`, `notification_id`, `deletedAt`) VALUES (NULL, ?, ?, ?, ?, ?, NULL);',
+    //         [data.senderId, data.chatRoomId, data.chatType, 1, notificationId],
+    //         (err, results, fields) => {
+    //             if (err) console.log(err);
+    //         }
+    //     );
+    // }
+}
+
+module.exports = MySql;
--- server/index.js
+++ server/index.js
@@ -2,13 +2,28 @@ const Const = require('../Const');
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('./')(server, { origins: '*:*'});
+const io = require('./')(server, {origins: '*:*'});
 const port = process.env.PORT || Const.port;
+
+const MySql = require('./class/MySql');
+
+const mySql = new MySql();
 
 server.listen(port, () => {
     console.log('Server listening at port %d', port);
 });
 
 io.on('connection', (socket) => {
-    console.log(1);
+
+    socket.on('event', (data, fn) => {
+        switch (data.name) {
+            case 'all_events':
+                mySql.getAllEvents((allEvents) => {
+                    socket.emit('event', {name: 'all_events', value: allEvents});
+                });
+                break;
+            default:
+                console.log("no action define", data);
+        }
+    });
 });
