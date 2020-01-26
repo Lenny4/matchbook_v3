f1f4575141d6da7373ffd660e9aa776b05c7e755
Lenny4 <alexandre.beaujour@hotmail.fr>
2020-01-26 14:56:42 +0100

    can display chart in separate file

--- assets/js/class/ChartManager.js
+++ assets/js/class/ChartManager.js
@@ -2,13 +2,15 @@
 const clone = require('clone');
 const randomstring = require("randomstring");
 
+const Function = require("../../../function/Function");
+
 class ChartManager {
     constructor(socket, app) {
         this.app = app;
         this.socket = socket;
         this.pointLabel = {'type': 'string', 'role': 'style'};
-        this.pointValueUp = "point { size: 12; shape-type: star; fill-color: black; }";
-        this.pointValueDown = "point { size: 12; shape-type: star; fill-color: red; }";
+        this.pointValueUp = "point { size: 12; shape-type: star; fill-color: red; }";
+        this.pointValueDown = "point { size: 12; shape-type: star; fill-color: blue; }";
         this.options = {
             title: null,
             curveType: 'function',
@@ -34,40 +36,15 @@ class ChartManager {
 
         //region create charts
         match.markets.runners.forEach((runner) => {
-            const fieldsBack = [['time', runner.name, this.pointLabel, "availableAmount1", "availableAmount2", "availableAmount3", "availableAmount4"]];
-            const dataBack = [];
+            const fieldsBack = ['time', runner.name, this.pointLabel, "availableAmount1", "availableAmount2", "availableAmount3", "availableAmount4"];
+            const fieldsBack2 = ['time', runner.name, this.pointLabel];
 
-            const fieldsBack2 = [['time', runner.name]];
-            const dataBack2 = [];
+            const dataFormatedArray = Function.formatData(fieldsBack, runner, true, [3, 4, 5, 6], 400);
+            const dataFormatedArray2 = Function.formatData(fieldsBack2, runner);
 
-            runner.prices.forEach((price) => {
+            const bets = Function.findTopAndBottom(dataFormatedArray);
 
-                const backPrices = price.value.filter(x => x.side === "back");
-                if (Array.isArray(backPrices) && backPrices.length > 0) {
-                    const currentMaxOddBack = backPrices.reduce((prev, current) => {
-                        return (prev.odds > current.odds) ? prev : current
-                    }).odds;
-
-                    let currentAvailableAmount1 = null;
-                    if ("0" in backPrices) currentAvailableAmount1 = backPrices[0]["available-amount"];
-
-                    let currentAvailableAmount2 = null;
-                    if ("1" in backPrices) currentAvailableAmount2 = backPrices[1]["available-amount"];
-
-                    let currentAvailableAmount3 = null;
-                    if ("2" in backPrices) currentAvailableAmount3 = backPrices[2]["available-amount"];
-
-                    let currentAvailableAmount4 = null;
-                    if ("3" in backPrices) currentAvailableAmount4 = backPrices[3]["available-amount"];
-
-                    dataBack.push([price.time, currentMaxOddBack, null, currentAvailableAmount1, currentAvailableAmount2, currentAvailableAmount3, currentAvailableAmount4]);
-                    dataBack2.push([price.time, currentMaxOddBack]);
-                }
-            });
-            const dataFormatedArray = this.formatData(fieldsBack.concat(dataBack), true, [3, 4, 5, 6], 400);
-            const dataFormatedArray2 = this.formatData(fieldsBack2.concat(dataBack2));
-
-            this.findTopAndBottom(dataFormatedArray, runner.name);
+            this.displayBetsOnChart(dataFormatedArray, bets);
 
             this.addChartToDisplayChart(result, runner.name, dataFormatedArray2);
             this.addChartToDisplayChart(result, runner.name, dataFormatedArray);
@@ -77,87 +54,13 @@ class ChartManager {
         return result;
     }
 
-    findTopAndBottom(data, runnerName) {
-        let lastTopBottom = null;
-        let nbBackOddGoUp = 0;
-
-        let goingUp1 = false;
-
-        data.forEach((array, index) => {
-            if (index > 1000) {
-                const time = array[0];
-                const backOdd = array[1];
-                const availableAmount1 = array[3];
-                const availableAmount2 = array[4];
-                const availableAmount3 = array[5];
-                const availableAmount4 = array[6];
-
-                let goingUp = false;
-                let goingDown = false;
-
-                if (backOdd > data[index - 1][1]) {
-                    nbBackOddGoUp++;
-                }
-
-                //region goingUp1
-                /**
-                 * dans le cas ou availableAmount2 >= backOdd && availableAmount3 >= backOdd
-                 * dès qu'ils repassent en dessous de backOdd, si le availableAmount1 n'est pas allé au dessus de backOdd
-                 * sur les 100 dernière secondes
-                 * on fait un back dès que la côte augmente 2 fois de suite
-                 */
-                if (
-                    (availableAmount2 >= backOdd && availableAmount3 >= backOdd)
-                    && goingUp1 === false
-                ) {
-                    goingUp1 = true;
-                    for (let i = index; i >= index - 100; i--) {
-                        if (data[i][3] > backOdd) {
-                            goingUp1 = false;
-                            break;
-                        }
-                    }
-                }
-                if (
-                    goingUp1 === true
-                    && (nbBackOddGoUp >= 2)
-                    && (backOdd > data[index - 1][1])
-                ) {
-                    goingUp1 = false;
-                    goingUp = true;
-                }
-                //endregion
-
-                //region goingUp2
-                /**
-                 * si availableAmount2 > backOdd && availableAmount4 > backOdd
-                 */
-                if (availableAmount2 > backOdd && availableAmount4 > backOdd) {
-                    goingUp = true;
-                }
-                //endregion
-
-                //region goingDown1
-                /**
-                 * si availableAmount1 > backOdd && availableAmount2 > backOdd && availableAmount3 < backOdd
-                 */
-                if (availableAmount1 > backOdd && availableAmount2 > backOdd && availableAmount3 < backOdd) {
-                    goingDown = true;
-                }
-                //endregion
-
-                if (goingUp && lastTopBottom !== "up") {
-                    array[2] = this.pointValueUp;
-                    lastTopBottom = "up";
-                }
-                if (goingDown && lastTopBottom !== "bottom") {
-                    array[2] = this.pointValueDown;
-                    lastTopBottom = "bottom";
-                }
-
-                if (backOdd < data[index - 1][1]) {
-                    nbBackOddGoUp = 0;
-                }
+    displayBetsOnChart(dataFormatedArray, bets) {
+        bets.forEach((bet) => {
+            const array = dataFormatedArray.find(x => x[0] === bet.time);
+            if (bet.side === "back") {
+                array[2] = this.pointValueDown;
+            } else {
+                array[2] = this.pointValueUp;
             }
         });
     }
@@ -187,51 +90,6 @@ class ChartManager {
             title: title,
             chart: GoogleCharts.api.visualization.arrayToDataTable(data),
         });
-    }
-
-    formatData(data, reduceTo1 = false, indexToFlat = [], numberFlat = 100) {
-        if (reduceTo1 === true) {
-            const numbersIndex = this.findIndexOfNumbers(data[0]);
-            numbersIndex.forEach((i) => {
-                let max = 1;
-                data.forEach((array, index) => {
-                    if (index > 0) {
-                        if (array[i] > max) {
-                            max = array[i];
-                        }
-                        array[i] = array[i] / max;
-                    }
-                });
-            });
-        }
-        if (indexToFlat.length > 0) {
-            for (let nbFlat = 0; nbFlat < numberFlat; nbFlat++) {
-                for (let i = 0; i < data.length; i++) {
-                    if (i > 0) {
-                        indexToFlat.forEach((thisIndexToFlat) => {
-                            if (i === 1) {
-                                data[i][thisIndexToFlat] = ((data[i][thisIndexToFlat] * 2) + data[i + 1][thisIndexToFlat]) / 3;
-                            } else if (i === data.length - 1) {
-                                data[i][thisIndexToFlat] = ((data[i][thisIndexToFlat] * 2) + data[i - 1][thisIndexToFlat]) / 3;
-                            } else {
-                                data[i][thisIndexToFlat] = ((data[i][thisIndexToFlat] * 2) + data[i + 1][thisIndexToFlat] + data[i - 1][thisIndexToFlat]) / 4;
-                            }
-                        });
-                    }
-                }
-            }
-        }
-        return data;
-    }
-
-    findIndexOfNumbers(array) {
-        const idxs = [];
-        for (let i = array.length - 1; i >= 0; i--) {
-            if (typeof array[i] === "string" && array[i] !== "time") {
-                idxs.unshift(i);
-            }
-        }
-        return idxs;
     }
 
     getOptions() {
---
+++ function/Function.js
@@ -0,0 +1,186 @@
+"use strict";
+
+function findIndexOfNumbers(array) {
+    const idxs = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        if (typeof array[i] === "string" && array[i] !== "time") {
+            idxs.unshift(i);
+        }
+    }
+    return idxs;
+}
+
+const Function = {
+    formatData(fields, runner, reduceTo1 = false, indexToFlat = [], numberFlat = 100) {
+        let data = [];
+        runner.prices.forEach((price) => {
+
+            const backPrices = price.value.filter(x => x.side === "back");
+            if (Array.isArray(backPrices) && backPrices.length > 0) {
+                const currentMaxOddBack = backPrices.reduce((prev, current) => {
+                    return (prev.odds > current.odds) ? prev : current
+                }).odds;
+                const arrayRunner = [price.time, currentMaxOddBack, null];
+
+                if (fields[3] === "availableAmount1") {
+                    let currentAvailableAmount1 = null;
+                    if ("0" in backPrices) currentAvailableAmount1 = backPrices[0]["available-amount"];
+                    arrayRunner.push(currentAvailableAmount1);
+                }
+                if (fields[4] === "availableAmount2") {
+                    let currentAvailableAmount2 = null;
+                    if ("1" in backPrices) currentAvailableAmount2 = backPrices[1]["available-amount"];
+                    arrayRunner.push(currentAvailableAmount2);
+                }
+                if (fields[5] === "availableAmount3") {
+                    let currentAvailableAmount3 = null;
+                    if ("2" in backPrices) currentAvailableAmount3 = backPrices[2]["available-amount"];
+                    arrayRunner.push(currentAvailableAmount3);
+                }
+                if (fields[6] === "availableAmount4") {
+                    let currentAvailableAmount4 = null;
+                    if ("3" in backPrices) currentAvailableAmount4 = backPrices[3]["available-amount"];
+                    arrayRunner.push(currentAvailableAmount4);
+                }
+
+                data.push(arrayRunner);
+            }
+        });
+
+        data = [fields].concat(data);
+        if (reduceTo1 === true) {
+            const numbersIndex = findIndexOfNumbers(data[0]);
+            numbersIndex.forEach((i) => {
+                let max = 1;
+                data.forEach((array, index) => {
+                    if (index > 0) {
+                        if (array[i] > max) {
+                            max = array[i];
+                        }
+                        array[i] = array[i] / max;
+                    }
+                });
+            });
+        }
+        if (indexToFlat.length > 0) {
+            for (let nbFlat = 0; nbFlat < numberFlat; nbFlat++) {
+                for (let i = 0; i < data.length; i++) {
+                    if (i > 0) {
+                        indexToFlat.forEach((thisIndexToFlat) => {
+                            if (i === 1) {
+                                data[i][thisIndexToFlat] = ((data[i][thisIndexToFlat] * 2) + data[i + 1][thisIndexToFlat]) / 3;
+                            } else if (i === data.length - 1) {
+                                data[i][thisIndexToFlat] = ((data[i][thisIndexToFlat] * 2) + data[i - 1][thisIndexToFlat]) / 3;
+                            } else {
+                                data[i][thisIndexToFlat] = ((data[i][thisIndexToFlat] * 2) + data[i + 1][thisIndexToFlat] + data[i - 1][thisIndexToFlat]) / 4;
+                            }
+                        });
+                    }
+                }
+            }
+        }
+        return data;
+    },
+
+    findTopAndBottom(data) {
+        const bets = [];
+
+        let lastTopBottom = null;
+        let nbBackOddGoUp = 0;
+
+        let goingUp1 = false;
+
+        data.forEach((array, index) => {
+            if (index > 1000) {
+                let nameBet = null;
+                const time = array[0];
+                const backOdd = array[1];
+                const availableAmount1 = array[3];
+                const availableAmount2 = array[4];
+                const availableAmount3 = array[5];
+                const availableAmount4 = array[6];
+
+                let goingUp = false;
+                let goingDown = false;
+
+                if (backOdd > data[index - 1][1]) {
+                    nbBackOddGoUp++;
+                }
+
+                //region goingUp1
+                /**
+                 * dans le cas ou availableAmount2 >= backOdd && availableAmount3 >= backOdd
+                 * dès qu'ils repassent en dessous de backOdd, si le availableAmount1 n'est pas allé au dessus de backOdd
+                 * sur les 100 dernière secondes
+                 * on fait un back dès que la côte augmente 2 fois de suite
+                 */
+                if (
+                    (availableAmount2 >= backOdd && availableAmount3 >= backOdd)
+                    && goingUp1 === false
+                ) {
+                    goingUp1 = true;
+                    for (let i = index; i >= index - 100; i--) {
+                        if (data[i][3] > backOdd) {
+                            goingUp1 = false;
+                            break;
+                        }
+                    }
+                }
+                if (
+                    goingUp1 === true
+                    && (nbBackOddGoUp >= 2)
+                    && (backOdd > data[index - 1][1])
+                ) {
+                    goingUp1 = false;
+                    nameBet = "goingUp1";
+                    goingUp = true;
+                }
+                //endregion
+
+                //region goingUp2
+                /**
+                 * si availableAmount2 > backOdd && availableAmount4 > backOdd
+                 */
+                if (availableAmount2 > backOdd && availableAmount4 > backOdd) {
+                    goingUp = true;
+                    nameBet = "goingUp2";
+                }
+                //endregion
+
+                //region goingDown1
+                /**
+                 * si availableAmount1 > backOdd && availableAmount2 > backOdd && availableAmount3 < backOdd
+                 */
+                if (availableAmount1 > backOdd && availableAmount2 > backOdd && availableAmount3 < backOdd) {
+                    goingDown = true;
+                    nameBet = "goingDown";
+                }
+                //endregion
+
+                const bet = {
+                    side: null,
+                    time: time,
+                    name: nameBet,
+                };
+
+                if (goingUp && lastTopBottom !== "lay") {
+                    lastTopBottom = "lay";
+                    bet.side = "lay";
+                    bets.push(bet);
+                }
+                if (goingDown && lastTopBottom !== "back") {
+                    lastTopBottom = "back";
+                    bet.side = "back";
+                    bets.push(bet);
+                }
+
+                if (backOdd < data[index - 1][1]) {
+                    nbBackOddGoUp = 0;
+                }
+            }
+        });
+        return bets;
+    }
+};
+
+module.exports = Function;
